2f1a06334c31623e9913515e128adf649939b34a
Maksym Tychyna <Maksym.Tychyna@thomsonreuters.com>
2019-08-01 15:10:12 +0300

    RDS

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ git reset --hard
 git checkout origin/master
 git fetch origin
 mvn clean package
-java -jar target/spring-boot-aws.jar
+java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000 target/spring-boot-aws.jar
--- src/main/java/com/maks/controller/AwsFileUploadController.java
+++ src/main/java/com/maks/controller/AwsFileUploadController.java
@@ -1,5 +1,7 @@
 package com.maks.controller;
 
+import com.maks.repository.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class AwsFileUploadController {
+
+    @Autowired
+    private FileRepository fileRepository;
 
     @GetMapping("/")
     public String list(Model model) {
